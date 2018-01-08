4a82439e7a973f129791c155d48899cd13255167
Anticolick <Anticolick@users.noreply.github.com>
2018-01-08 04:57:50 +0300

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+  #define MAKS 100
+  using namespace std;
+
+  int main()
+  {
+   cout<<"yaz yavrum ben ters cevirip sana sokcam\n";
+   char cumle[MAKS];
+  	char tersi[MAKS];
+  	cin.getline(cumle, MAKS - 1);
+
+  	int i = -1, t = 0, basi = 0, sonu;
+  	do {
+  		++i;
+  		for(; cumle[i] != ' ' && cumle[i] != '\0'; i++);
+  		sonu = i - 1;
+
+  		while(sonu >= basi) tersi[t++] = cumle[sonu--];
+  		tersi[t++] = ' ';
+
+  		basi = i + 1;
+  	} while(cumle[i]);
+  	tersi[t] = '\0';
+
+  	cout << tersi << '\n';
+
+  	return 0;
+  }
